8567622e3e70b315a7f5b1e582535f5ae1143091
kabah <1kabah@gmail.com>
2016-06-09 11:34:40 -0700

    requires model in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,6 @@
 # Memory.destroy_all
+require_relative '../models/memory'
+require 'faker'
 
 10.times do 
 	m = Memory.new
@@ -23,6 +25,6 @@ end
 # end
 
 
-Memory.create(name: 'User One', title: 'I went into labor', content: 'I gave birth to my five kids...at the same time. QUINTUPLETS!')
-Memory.create(name: 'User Two', title: 'We had the most fun ever', content: 'We went to that super fun place, and had the most fun ever')
-Memory.create(name: 'User Three', title: 'We had an unexpected adventure', content: "We planed to have a totally regular day, but somehow some crazy things happened, but it was awesome. I'll never forget")
+# Memory.create(name: 'User One', title: 'I went into labor', content: 'I gave birth to my five kids...at the same time. QUINTUPLETS!')
+# Memory.create(name: 'User Two', title: 'We had the most fun ever', content: 'We went to that super fun place, and had the most fun ever')
+# Memory.create(name: 'User Three', title: 'We had an unexpected adventure', content: "We planed to have a totally regular day, but somehow some crazy things happened, but it was awesome. I'll never forget")
