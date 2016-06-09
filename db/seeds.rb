c3ccf397f54c16227504e6204accdde28e7275bd
kabah <1kabah@gmail.com>
2016-06-09 11:17:52 -0700

    config db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
 # Memory.destroy_all
 
-# 10.times do 
-# 	m = Memory.new
-# 	m.name = Faker::Name.first_name
-# 	m.title = Faker::Lorem.sentence
-# 	m.content = Faker::Lorem.paragraph(8)
-# 	m.created_at =Faker::Date.forward(365)
+10.times do 
+	m = Memory.new
+	m.name = Faker::Name.first_name
+	m.title = Faker::Lorem.sentence
+	m.content = Faker::Lorem.paragraph(8)
+	m.created_at =Faker::Date.forward(365)
 
-# 	m.save
-# end
+	m.save
+end
 
 # 14.times do
 
@@ -23,6 +23,6 @@
 # end
 
 
-# Memory.create(name: 'User One', title: 'I went into labor', content: 'I gave birth to my five kids...at the same time. QUINTUPLETS!')
-# Memory.create(name: 'User Two', title: 'We had the most fun ever', content: 'We went to that super fun place, and had the most fun ever')
-# Memory.create(name: 'User Three', title: 'We had an unexpected adventure', content: "We planed to have a totally regular day, but somehow some crazy things happened, but it was awesome. I'll never forget")
+Memory.create(name: 'User One', title: 'I went into labor', content: 'I gave birth to my five kids...at the same time. QUINTUPLETS!')
+Memory.create(name: 'User Two', title: 'We had the most fun ever', content: 'We went to that super fun place, and had the most fun ever')
+Memory.create(name: 'User Three', title: 'We had an unexpected adventure', content: "We planed to have a totally regular day, but somehow some crazy things happened, but it was awesome. I'll never forget")
